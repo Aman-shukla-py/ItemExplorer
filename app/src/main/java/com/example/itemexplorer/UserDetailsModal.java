00ecebeb30143a436f505ec03b6d476198fa4779
Aman-shukla-py <Aman123shukla18@gmail.com>
2023-11-20 13:41:15 +0530

    updated

---
+++ app/src/main/java/com/example/itemexplorer/UserDetailsModal.java
@@ -0,0 +1,15 @@
+package com.example.itemexplorer;
+
+public class UserDetailsModal {
+    public String deptname, email;
+
+    public UserDetailsModal(){
+
+    }
+
+    public UserDetailsModal(String name, String email) {
+        this.deptname = name;
+        this.email = email;
+
+    }
+}
